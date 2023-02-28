359b6bba460bd972408f2e67853652d2c4dc92f5
Ahmedabdullahi1 <Ahmedkade@hotmail.com>
2023-02-28 01:08:24 +0000

    adding completed work

--- Pybank/main.py
+++ Pybank/main.py
@@ -1,7 +1,59 @@
-import csv
 import os
-#path to collect datat from resources folder
-csvpath = os.path.join("Resources","budget_data.csv")
+import csv
 
-#read csv file
-with open(csv, 'r') as csv file
+# Assign the csv path to a variable
+budgetDataPath = "Resources/budget_data.csv"
+
+# Read csv file
+with open(budgetDataPath, "r", encoding="utf8") as budgetData:
+    budgetDataReading = csv.reader(budgetData, delimiter=",")
+    budgetDataHeader = next(budgetDataReading)
+    fullBudget = []
+    monthlyValues = []
+    monthsCount = 0
+    netTotal = 0
+    
+    # Iterate through csv data and append both the full budget and only values' lines to variables
+    for line in budgetDataReading:
+        monthsCount += 1
+        netTotal += int(line[1])
+        fullBudget.append(line)
+        monthlyValues.append(line[1])
+
+# initialise and empty list to hold the monthly changes later
+monthlyChanges = []
+
+# iterate through only the monthly values and append the calculated changes to "MonthlyChanges" variable
+for index, amount in enumerate(monthlyValues):
+    if index < len(monthlyValues) and index > 0:
+        monthlyChanges.append(int(amount) - int(monthlyValues[index - 1]))
+
+# Perform Average Change operation
+averageChange = round(sum(monthlyChanges)/len(monthlyChanges), 2)
+
+# Iterate through Monthly Changes and deduce the Greatest Increase and Greatest Decrease values
+for index, singleChange in enumerate(monthlyChanges):
+    if singleChange == max(monthlyChanges):
+        greatestIncrease = fullBudget[index + 1]
+    # Revisit with else
+
+    if singleChange == min(monthlyChanges):
+        greatestDecrease = fullBudget[index + 1]
+    # Revisit with else
+
+# Final Analysis of Budget
+analysis = f'''
+    Financial Analysis
+    ----------------------------
+    Total Months: {monthsCount}
+    Total: ${netTotal}
+    Average Change: ${averageChange}
+    Greatest Increase in Profits: {greatestIncrease[0]} (${max(monthlyChanges)})
+    Greatest Decrease in Profits: {greatestDecrease[0]} (${min(monthlyChanges)})
+'''
+print(analysis)
+
+# export analysis to "analysis.txt" text file
+analysisPath = "analysis/analysis.txt"
+with open(analysisPath, "w") as analysisFile:
+    analysisFile.write(analysis)
--- Pypoll/main.py
+++ Pypoll/main.py
@@ -1,8 +1,60 @@
-import csv
 import os
-#path to collect datat from resources folder
-csvpath = os.path.join("Resources","budget_data.csv")
+import csv
 
-#read csv file
-with open(csvpath, 'r') as csvfile:
+os.system("cls")
+
+# Assign the csv path to a variable
+electionDataPath = "Resources/election_data.csv"
+
+electionDataList = []
+totalVotes = 0
+
+# Read csv file
+with open(electionDataPath, "r", encoding="utf8") as electionData:
+    readingElectionData = csv.reader(electionData, delimiter=",")
+    electionDataHeader = next(readingElectionData)
     
+    # Iterate through csv data and append all lines to variable
+    for line in readingElectionData:
+        totalVotes += 1
+        electionDataList.append(line)
+
+# Grab Voting List by only Candidate Names
+votesByCandidateName = [v[2] for v in electionDataList]
+
+# Initialise empty list to subsequently hold only names of all candidates
+candidateList = []
+[candidateList.append(li) for li in votesByCandidateName if li not in candidateList]
+
+# Append Candidate's name, Winning percentage and Vote count to List for use later
+candidateSummary = []
+[candidateSummary.append([c, round((((votesByCandidateName.count(c)) / totalVotes) * 100), 3), votesByCandidateName.count(c)]) for c in candidateList]
+
+# Compute Winner of Election
+def winner():
+    groupedCount = []
+    [groupedCount.append(c[2]) for c in candidateSummary]
+    MaxCount = [i[0] for i in candidateSummary if max(groupedCount) == i[2]]
+    return MaxCount[0]
+
+# Capture the Candidate's name, Winning percentage and Vote count to a variable for use in the Final analysis
+summary = ""
+for t in range(len(candidateList)):
+    summary += f"    {candidateSummary[t][0]}: {candidateSummary[t][1]}% ({candidateSummary[t][2]})\n"
+
+# Final Analysis of Elections
+analysis = f'''
+    Election Results
+    -------------------------
+    Total Votes: {totalVotes}
+    -------------------------
+{summary}    -------------------------
+    Winner: {winner()}
+    -------------------------'''
+print(analysis)
+
+
+# Export analysis to "analysis.txt" text file
+analysisPath = "analysis/analysis.txt"
+with open(analysisPath, "w") as analysisFile:
+    analysisFile.write(analysis)
